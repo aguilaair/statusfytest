7e9e529fbd36734e9ce4ddcd21b8fb1356659312
Aguilaair <eduardomoreno.eduardo@gmail.com>
2019-07-03 11:06:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  title: 'System Status',
-  name: 'system_status',
-  description: 'Now - Static Generated Example'
-};
-defaultLocale: 'en',
+  title: 'Title',
+  description: 'Description',
+  defaultLocale: 'en',
   locales: [
     { code: 'en', iso: 'en-US', name: 'English' },
     { code: 'es', iso: 'es-ES', name: 'Español' }
@@ -21,10 +19,5 @@ defaultLocale: 'en',
     scheduled: {
       position: 'aboveGlobalStatus'
     }
-  },
-  head: {
-    link: [
-      { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3e4e88' }
-    ]
   }
 }
