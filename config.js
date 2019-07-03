a90c0d20a6cf30dd5cdd11a005b3720e38bb9829
Aguilaair <eduardomoreno.eduardo@gmail.com>
2019-07-03 11:11:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 module.exports = {
   title: 'Title',
   description: 'Description',
-  defaultLocale: 'en',
-  locales: [
-    { code: 'en', iso: 'en-US', name: 'English' },
-    { code: 'es', iso: 'es-ES', name: 'Español' }
-  ],
   content: {
     frontMatterFormat: 'yaml',
     systems: [
